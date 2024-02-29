d259f930e258a0562996cbba536cb41df0cebd99
Shwerm <95546677+Shwerm@users.noreply.github.com>
2024-02-29 18:36:28 +0100

    Initial Setup
     Test Header and Cpp

---
+++ DollHouse/src/Test.cpp
@@ -0,0 +1,10 @@
+#include "Test.h"
+#include <stdio.h>
+
+namespace DollHouse {
+
+	void Print()
+	{
+		printf("Welcome to the Doll House Engine");
+	}
+}
---
+++ DollHouse/src/Test.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace DollHouse {
+
+	__declspec(dllexport)void Print();
+
+}
