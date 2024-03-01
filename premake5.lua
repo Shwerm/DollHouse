093cc185b61286ce714a7e2758994c720fd7248b
Shwerm <95546677+Shwerm@users.noreply.github.com>
2024-03-01 12:30:14 +0100

    Premake Implemented
    Premake 5 Bat and .lua make file written for copying .dll file to sandbox directory

---
+++ premake5.lua
@@ -0,0 +1,107 @@
+workspace "DollHouse"
+	architecture "x64"
+
+	configurations 
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "DollHouse"
+	location "DollHouse"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include;"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17" 
+		staticruntime "On"
+		systemversion "10.0"
+
+		defines
+		{
+			"DH_PLATFORM_WINDOWS",
+			"DH_BUILD_DLL",
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "DH_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "DH_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "DH_DIST"
+		optimize "On"
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"DollHouse/vendor/spdlog/include;",
+		"DollHouse/src"
+	}
+
+	links
+	{
+		"DollHouse"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17" 
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"DH_PLATFORM_WINDOWS",
+		}
+
+	filter "configurations:Debug"
+		defines "DH_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "DH_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "DH_DIST"
+		optimize "On"
+
